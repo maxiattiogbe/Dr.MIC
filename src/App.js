00cd82b0b21c6ceb793001d856002f7201a0f5fe
Maxi Attiogbe <maxiattiogbe@gmail.com>
2024-01-26 17:14:15 -0500

    Home page text change and default logo removal

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <p>
+         Welcome to Dr. MIC's office!
+        </p>
+        <br></br>
+        <p>
+          Upload an image below to get a diagnosis from Dr. MIC (pronounced Mike and short for Medical Image Classifier).
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
